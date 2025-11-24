33c706055e33bf0a9ad351953a0a3e573d4802b8
luismarcondesmed-art <luismarcondes.med@gmail.com>
2025-11-24 13:22:41 -0300

    Add service worker for caching static resources

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'resiflow-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/lucide@latest'
+];
+
+// Instalação: Cacheia os recursos estáticos
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+// Ativação: Limpa caches antigos se necessário
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptação: Cache First, Network Fallback (para libs) ou Network First (para dados)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Retorna do cache se existir
+        if (response) {
+          return response;
+        }
+        // Se não, busca na rede
+        return fetch(event.request);
+      })
+  );
+});
