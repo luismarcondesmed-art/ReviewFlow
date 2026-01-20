214ab3b1905928a8601c678d1c51a0028e936d63
Luis Gustavo Silva Marcondes <luis.gustavomarcondes07@gmail.com>
2026-01-20 17:08:32 -0300

    Preparando para deploy Vercel

--- views.tsx
+++
@@ -1,6 +0,0 @@
-export * from './view-hub';
-export * from './view-calendar';
-export * from './view-database';
-export * from './view-cronograma';
-export * from './view-analytics';
-export * from './view-simulados';
